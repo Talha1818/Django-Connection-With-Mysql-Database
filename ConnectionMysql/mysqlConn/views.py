f2e21a37070c3b924f95ef0d6aa627edd36af4a8
Talha1818 <muhammadtaha171717@gmail.com>
2022-07-30 23:03:22 +0500

    get data from mysql and display

--- ConnectionMysql/mysqlConn/views.py
+++ ConnectionMysql/mysqlConn/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from .models import Employee
 # Create your views here.
+def index(request):
+    emp = Employee.objects.all()
+    details = []
+    for i in emp:
+        name = i.name
+        email = i.email
+        address = i.address
+        details.append((name,email, address))
+    return HttpResponse(f"{details}")
+    # e = Employee(name='faisal', email='ta@gmail.com', address='Okay great!')
+    # e.save()
+
+    # print(emp.get('name'))
